487e89da866c9fd90454724ef1ab19f387d9fce1
Vitor Santos <vitorsantos45@live.com>
2023-03-09 02:03:56 -0300

    Projeto DIO

---
+++ src/main/java/one/digitalinnovation/gof/LabPadroesProjetosSpringApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPadroesProjetosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetosSpringApplication.class, args);
+	}
+
+}
